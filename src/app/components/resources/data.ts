5689b32bf1557eba826592d85cdb9496571d7f07
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-04-28 15:54:43 +0200

    added a animate css lib and animations in hero component for svg images

--- src/app/components/resources/data.ts
+++ src/app/components/resources/data.ts
@@ -1,3 +1,233 @@
 export const RESOURCES = [
-
+  {
+    id: 1,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 2,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 3,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 4,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 5,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 6,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 7,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 8,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 9,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 10,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 11,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 12,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 13,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 14,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 15,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 16,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 17,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 18,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 19,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 20,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 21,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 22,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 23,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 24,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 25,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 26,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 27,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 28,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 29,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 30,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 31,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 32,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
+  {
+    id: 33,
+    title: '',
+    description: '',
+    link: '',
+    image: ''
+  },
 ];
