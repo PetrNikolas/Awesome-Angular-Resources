7ebe8204a488d822869692d6cc3a3f9bde0b1aeb
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-05-21 19:50:14 +0200

    add a google adsense support and license and contributing file

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { AdsenseModule } from 'ng2-adsense';
 
 // Components
 import { AppComponent } from './app.component';
@@ -13,7 +14,12 @@ import * as pipes from './pipes';
 export const IMPORTS = [
   BrowserModule,
   FormsModule,
-  ScrollingModule
+  ScrollingModule,
+  // shown passing global defaults (optional)
+  /**AdsenseModule.forRoot({
+    adClient: 'ca-pub-7640562161899788',
+    adSlot: 7259870550,
+  }),*/
 ];
 
 export const PROVIDERS = [];
