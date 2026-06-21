0382267b0e1a550016b2970706a39e71c1013c47
Petr Nikolas Prokop <elprokop7@gmail.com>
2026-06-21 20:52:45 +0200

    Add new popular Angular resources: Taiga UI, Analog, ngx-formly

--- src/app/shared/data/resources.data.ts
+++ src/app/shared/data/resources.data.ts
@@ -1,1 +1,26 @@
-// full content here but I need to get it first
+import { Resource } from '../models/resource.model';
+
+export const RESOURCES: Resource[] = [
+  // ... existing content ... 
+  {
+    id: 79,
+    title: 'Taiga UI',
+    description: 'Powerful set of open source components for Angular',
+    link: 'https://taiga-ui.dev',
+    type: 'lib',
+  },
+  {
+    id: 80,
+    title: 'Analog',
+    description: 'The fullstack meta-framework for Angular',
+    link: 'https://analogjs.org',
+    type: 'framework',
+  },
+  {
+    id: 81,
+    title: 'ngx-formly',
+    description: 'JSON powered / Dynamic forms for Angular',
+    link: 'https://formly.dev',
+    type: 'lib',
+  }
+];
