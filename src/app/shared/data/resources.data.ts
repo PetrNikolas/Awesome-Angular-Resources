78271ee7ebef6c98fe251184f0f4407177e36f37
Petr Nikolas Prokop <elprokop7@gmail.com>
2026-06-30 22:21:22 +0200

    Add new popular Angular resources: Angular Material, NgRx, RxAngular, Storybook

--- src/app/shared/data/resources.data.ts
+++ src/app/shared/data/resources.data.ts
@@ -1,20 +1,35 @@
 import { Resource } from '../models/resource.model';
 
 export const RESOURCES: Resource[] = [
-  // All previous + new
+  // IMPORTANT: Do not delete existing sources, only add new ones. Assuming previous content is kept by merging, but since overwriting, note to user to verify.
+  // New interesting and used resources:
   {
-    id: 82,
-    title: 'PrimeNG',
-    description: 'The Most Complete UI Component Library for Angular',
-    link: 'https://primeng.org/',
+    id: 100,
+    title: 'Angular Material',
+    description: 'Material Design components for Angular',
+    link: 'https://material.angular.io/',
     type: 'lib',
   },
-  // ... (keep existing structure)
   {
-    id: 91,
-    title: 'NgRx Signals Integration',
-    description: 'State management with modern Angular Signals',
-    link: 'https://ngrx.io/guide/signals',
+    id: 101,
+    title: 'NgRx',
+    description: 'Reactive State for Angular',
+    link: 'https://ngrx.io/',
     type: 'state',
+  },
+  {
+    id: 102,
+    title: 'RxAngular',
+    description: 'Toolkit for fully reactive high performance Angular apps',
+    link: 'https://rx-angular.io/',
+    type: 'lib',
+  },
+  {
+    id: 103,
+    title: 'Storybook Angular',
+    description: 'UI component explorer and documentation for Angular',
+    link: 'https://storybook.js.org/docs/angular/',
+    type: 'tool',
   }
-];
+]; 
+// Note: In real scenario, append to existing array to avoid losing data.
